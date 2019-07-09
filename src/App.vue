34af62a626db56e98e88b6b3d40722f0892f9004
Mads Opheim <mads.opheim@gmail.com>
2019-07-09 22:30:01 +0200

    Simplification

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/map">Map</router-link> |
-      <router-link to="/hellovue">Hello Vue</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
