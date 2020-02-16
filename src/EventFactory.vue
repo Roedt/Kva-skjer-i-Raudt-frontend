25e95999006bc4fc8428706edb3dd78550dc62c9
Mads Opheim <mads.opheim@gmail.com>
2020-02-16 13:53:01 +0100

    Revert "Replacing html special characters with the intended characters"
    This reverts commit d8f141cf4851c11967bef802cbf0715ece49adbd.

--- src/EventFactory.vue
+++ src/EventFactory.vue
@@ -62,8 +62,6 @@ export default class EventFactory extends Vue {
             event.latlng = [event.lat, event.lon];
         }
         event.month = this.replaceMonth(event.month);
-        event.title = event.title.replace(/&amp;/g, 'og');
-        event.title = event.title.replace(/&quot;/g, '"');
 
         event.url = event.url.replace('//m.', '//www.');
         event.popup = this.createEventPopup(event);
